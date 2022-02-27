a6b8d972307bbf15d0d9a39b12ffe5a80df485e6
Michał Jankowski <jankowski.nml@gmail.com>
2022-02-27 14:46:53 +0100

    Add log beta_0.1

--- blog/routes.py
+++ blog/routes.py
@@ -1,9 +1,19 @@
 # blog/routes.py
 
-from flask import render_template, request
+from flask import render_template, request, session, flash, redirect, url_for
 from blog import app
 from blog.models import Entry, db
-from blog.forms import EntryForm
+from blog.forms import EntryForm, LoginForm
+import functools
+
+
+def login_required(view_func):
+   @functools.wraps(view_func)
+   def check_permissions(*args, **kwargs):
+       if session.get('logged_in'):
+           return view_func(*args, **kwargs)
+       return redirect(url_for('login', next=request.path))
+   return check_permissions
 
 
 @app.route("/")
@@ -12,8 +22,10 @@ def index():
 
     return render_template("homepage.html", all_posts=all_posts)
 
+
 @app.route("/edit-entry/<int:entry_id>", methods=["GET", "POST"])
 @app.route("/edit-entry", methods=["GET", "POST"])
+@login_required
 def edit_entry(entry_id=None):
     errors = None
     if entry_id:
@@ -38,4 +50,29 @@ def edit_entry(entry_id=None):
             db.session.commit()
         else:
             errors = form.errors
+
     return render_template("entry_form.html", form=form, errors=errors)
+
+
+@app.route("/login/", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    errors = None
+    next_url = request.args.get('next')
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            session['logged_in'] = True
+            session.permanent = True  # Use cookie to store session.
+            flash('You are now logged in.', 'success')
+            return redirect(next_url or url_for('index'))
+        else:
+            errors = form.errors
+    return render_template("login_form.html", form=form, errors=errors)
+
+
+@app.route('/logout/', methods=['GET', 'POST'])
+def logout():
+    if request.method == 'POST':
+        session.clear()
+        flash('You are now logged out.', 'success')
+    return redirect(url_for('index'))
