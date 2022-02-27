0ac723773ff12c661e460824bf4623592138c79a
Michał Jankowski <jankowski.nml@gmail.com>
2022-02-27 13:26:48 +0100

    Add refactoring def edit_entry

--- blog/routes.py
+++ blog/routes.py
@@ -12,40 +12,9 @@ def index():
 
     return render_template("homepage.html", all_posts=all_posts)
 
-
-@app.route("/new-post/", methods=["GET", "POST"])
-def create_entry():
-    form = EntryForm()
-    errors = None
-    if request.method == 'POST':
-        if form.validate_on_submit():
-            entry = Entry(
-                title=form.title.data,
-                body=form.body.data,
-                is_published=form.is_published.data
-            )
-            db.session.add(entry)
-            db.session.commit()
-        else:
-            errors = form.errors
-    return render_template("entry_form.html", form=form, errors=errors)
-
-@app.route("/edit-post/<int:entry_id>", methods=["GET", "POST"])
-def edit_entry(entry_id):
-    entry = Entry.query.filter_by(id=entry_id).first_or_404()
-    form = EntryForm(obj=entry)
-    errors = None
-    if request.method == 'POST':
-        if form.validate_on_submit():
-            form.populate_obj(entry)
-            db.session.commit()
-        else:
-            errors = form.errors
-    return render_template("entry_form.html", form=form, errors=errors)
-
-@app.route("/edit-post4/<int:entry_id>", methods=["GET", "POST"])
-@app.route("/edit-post4", methods=["GET", "POST"])
-def create_edit_entry3(entry_id=None):
+@app.route("/edit-entry/<int:entry_id>", methods=["GET", "POST"])
+@app.route("/edit-entry", methods=["GET", "POST"])
+def edit_entry(entry_id=None):
     errors = None
     if entry_id:
         entry = Entry.query.filter_by(id=entry_id).first_or_404()
